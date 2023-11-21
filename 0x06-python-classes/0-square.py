9278cf568a083e2a07161643c5ae6511a9442cdb
FatimaFma <toomyox@gmail.com>
2023-11-21 04:25:48 -0800

    Task 1 Solution

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 """ Square class
 """
+
 class Square:
     """ simple class square without implementation
     """
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    square class module
+"""
+
+class Square:
+    """ Square object with size private instance attr
+    """
+
+    def __init__(self, size):
+        """ Square class instance initialization
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
